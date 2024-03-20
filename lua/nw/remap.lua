b4940231883b2f2f1497de7bcc93c3e74d77ef12
Enno Richter <enno@nerdworks.de>
2024-03-20 13:17:04 +0100

    add macos mappings

--- lua/nw/remap.lua
+++ lua/nw/remap.lua
@@ -43,6 +43,24 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
 
-vim.keymap.set("n", "<leader><leader>", function()
-	vim.cmd("so")
-end)
+--vim.keymap.set("n", "<leader><leader>", function()
+--	vim.cmd("so")
+--end)
+
+-- Option + Shift + w to insert „ (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-S-w>", "„", { noremap = true, silent = true })
+
+-- Option + [ to insert “ (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-[>", "“", { noremap = true, silent = true })
+
+-- Option + u followed by a,o,u,A,O,U to insert ä,ö,ü,Ä,Ö,Ü (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-u>a", "ä", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-u>o", "ö", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-u>u", "ü", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-u>A", "Ä", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-u>O", "Ö", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-u>U", "Ü", { noremap = true, silent = true })
+
+-- Option + s insert ß (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-s>", "ß", { noremap = true, silent = true })
+
