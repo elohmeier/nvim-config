d7b43cebccea15834a090b6d62539d5ec4818855
Enno Richter <enno@nerdworks.de>
2024-03-20 13:17:17 +0100

    add more exts

---
+++ lua/nw/lazy/ssr.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "cshuaimin/ssr.nvim",
+    config = function()
+        local ssr = require("ssr")
+        ssr.setup()
+
+        vim.keymap.set({ "n", "x" }, "<leader>rr", function() ssr.open() end)
+    end
+  },
+}
