996fabcf491ed11911bc4cd2fdba2b1ae819d0c3
Enno Richter <enno@nerdworks.de>
2024-06-10 10:18:48 +0200

    Enable additional language servers in the LSP configuration.

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,7 +3,15 @@
     enable = true;
 
     servers = {
+      gopls.enable = true;
+      lua-ls.enable = true;
       nil-ls.enable = true;
+      pyright.enable = true;
+      ruff-lsp.enable = true;
+      rust-analyzer.enable = true;
+      svelte.enable = true;
+      tailwindcss.enable = true;
+      tsserver.enable = true;
     };
   };
 }
