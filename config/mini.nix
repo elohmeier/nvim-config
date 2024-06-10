e2ff0983beb2602663c8f79d1b37bf16f8828187
Enno Richter <enno@nerdworks.de>
2024-06-10 05:51:59 +0200

    mini cursorwords: Remove logic to disable keywords except for 'neo-tree' filetype.

--- config/mini.nix
+++ config/mini.nix
@@ -9,32 +9,15 @@
     };
   };
 
-  # needs to be configured *before* calling module's `setup()`
-  extraConfigLuaPre = ''
+  extraConfigLua = ''
     local function cursorword_block()
-      local curword = vim.fn.expand('<cword>')
-      local filetype = vim.bo.filetype
 
       -- Always disable for 'neo-tree' filetype
       if filetype == 'neo-tree' then
         vim.b.minicursorword_disable = true
-        return
       end
 
-      -- Define keyword blocklist for specific file types
-      local blocklist = {
-        lua = { 'local', 'require' },
-        javascript = { 'import' }
-      }
-
-      local keywords = blocklist[filetype] or {}
-      vim.b.minicursorword_disable = vim.tbl_contains(keywords, curword)
     end
-
-    vim.api.nvim_create_autocmd("CursorMoved", {
-      pattern = "*",
-      callback = cursorword_block
-    })
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "*",
