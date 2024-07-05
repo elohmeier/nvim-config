e1c302863d0da689575960f08d851a9fab5352ad
Enno Richter <enno@nerdworks.de>
2024-07-05 06:43:23 +0200

    neo-tree: update keybindings

--- config/neo-tree.nix
+++ config/neo-tree.nix
@@ -17,30 +17,78 @@
       key = "<leader>fe";
       action.__raw = ''
         function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+          require("neo-tree.command").execute({ action = "focus", source = "filesystem", position = "left", dir = vim.loop.cwd() })
         end
       '';
       options = {
         silent = true;
-        desc = "Explorer NeoTree (cwd)";
+        desc = "Neo-tree file explorer (cwd)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fE";
+      action.__raw = ''
+        function()
+          local git_utils = require("neo-tree.git.utils")
+          require("neo-tree.command").execute({ action = "focus", source = "filesystem", position = "left", dir = git_utils.get_repository_root() })
+        end
+      '';
+      options = {
+        silent = true;
+        desc = "Neo-tree file explorer (git repo root)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>f.";
+      action.__raw = ''
+        function()
+          require("neo-tree.command").execute({ action = "focus", source = "filesystem", position = "left", dir = vim.fn.expand("%:p:h") })
+        end
+      '';
+      options = {
+        silent = true;
+        desc = "Neo-tree file explorer (current file parent)";
       };
     }
     {
       mode = "n";
       key = "<leader>ge";
-      action = ":Neotree git_status<CR>";
+      action.__raw = ''
+        function()
+          require("neo-tree.command").execute({ action = "focus", source = "git_status", position = "left" })
+        end
+      '';
       options = {
         silent = true;
-        desc = "Git explorer";
+        desc = "Neo-tree git explorer";
       };
     }
     {
       mode = "n";
       key = "<leader>be";
-      action = ":Neotree buffers<CR>";
+      action.__raw = ''
+        function()
+          require("neo-tree.command").execute({ action = "focus", source = "buffers", position = "left" })
+        end
+      '';
       options = {
         silent = true;
-        desc = "Buffer explorer";
+        desc = "Neo-tree buffer explorer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>de";
+      action.__raw = ''
+        function()
+          require("neo-tree.command").execute({ action = "focus", source = "document_symbols", position = "left" })
+        end
+      '';
+      options = {
+        silent = true;
+        desc = "Neo-tree document symbols explorer";
       };
     }
   ];
