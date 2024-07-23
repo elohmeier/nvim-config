8d69ebfaac9bd5811aeef4899c287af4bbdbfd82
Enno Richter <enno@nerdworks.de>
2024-07-23 06:14:18 +0200

    add luasnip-telescope

--- config/luasnip.nix
+++ config/luasnip.nix
@@ -1,17 +1,38 @@
-{ pkgs, ... }:
+{ pkgs, ... }: {
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "telescope-luasnip.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "benfowler";
+        repo = "telescope-luasnip.nvim";
+        rev = "11668478677de360dea45cf2b090d34f21b8ae07"; # 2024-06-08
+        hash = "sha256-jzLzucF2rAa3nkHE5n4g2StkSUjMOMK93JCH/g09DQY=";
+      };
+    })
+  ];
 
-{
   plugins.luasnip = {
     enable = true;
     extraConfig = {
       enable_autosnippets = true;
       store_selection_keys = "<Tab>";
     };
-    fromVscode = [
-      {
-        lazyLoad = true;
-        paths = "${pkgs.vimPlugins.friendly-snippets}";
-      }
-    ];
   };
+
+  plugins.friendly-snippets.enable = true;
+
+  plugins.telescope = {
+    enabledExtensions = [ "luasnip" ];
+  };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>fl";
+      action = "<cmd>Telescope luasnip<cr>";
+      options = {
+        desc = "Luasnip";
+      };
+    }
+  ];
 }
