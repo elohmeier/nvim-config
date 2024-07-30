026dda4b83fcdd65486915ea138d9749294b9a28
Enno Richter <enno@nerdworks.de>
2024-07-30 11:19:59 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
     flake-compat = {
       url = "github:edolstra/flake-compat";
