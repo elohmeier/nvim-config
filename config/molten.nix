6f674e80e9e2e1e1adbb3756930a66812bdb5900
Enno Richter <enno@nerdworks.de>
2024-11-21 09:40:34 +0100

    disable image.nvim

--- config/molten.nix
+++ config/molten.nix
@@ -1,15 +1,16 @@
 { minimal }:
 { lib, ... }:
 {
-  plugins.image = {
-    enable = !minimal;
-  };
+  # plugins.image = {
+  #   enable = !minimal;
+  # };
 
   plugins.molten = {
     enable = true;
     settings = {
       backend = "kitty";
-      image_provider = if minimal then "none" else "image.nvim";
+      # image_provider = if minimal then "none" else "image.nvim";
+      image_provider = "none";
     };
 
     python3Dependencies = lib.mkIf minimal (_: [ ]);
